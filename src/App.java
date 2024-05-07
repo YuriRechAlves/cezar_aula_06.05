4debcc670807f8683ebea0f9ef76a5539d623796
YuriRechAlves <yurirech2003@gmail.com>
2024-05-06 21:37:20 -0300

    Arquivo alterado

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
-
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Oi");
-    }
-    
+	public static void main (String[] args) {
+		System.out.println("Olá Pessoal");
+		System.out.println("Aula de git");
+		//comentário
+	}
 }
 
