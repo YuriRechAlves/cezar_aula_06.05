38bc01aed3b53f0dc55424434854d8234a63fb35
YuriRechAlves <yurirech2003@gmail.com>
2024-05-13 21:38:56 -0300

    hotfix: mensagem de olá

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,6 @@
 public class Pessoa{
 	public static void main(String[] args) {
-		System.out.println("Ola Pessoa!");
+		System.out.println("Ola Pessoal!");
 		System.out.println("Meu nome é Fulano!");
 	}
 }
